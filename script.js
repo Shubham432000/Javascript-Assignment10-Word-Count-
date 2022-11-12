199eab8867518f47b9d937c793bb232d847f0d8a
Shubham432000 <shubham7410112997@gmail.com>
2022-11-12 19:30:09 +0530

    commit

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const countwords = () => {
   let value1 = document.getElementById("data").value.length;
+  let value2 = document.getElementById("data").value
 
   console.log(value1);
 
   document.getElementById("data1").innerHTML = value1;
+
+ value2= value2.trim()
+ let word = value2.split(" ")
+
+ let clean = word.filter(function(ele){
+  return ele != " ";
+ })
+
+  document.getElementById("data2").innerHTML = clean.length
 };
